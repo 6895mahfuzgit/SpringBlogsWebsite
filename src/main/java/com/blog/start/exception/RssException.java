962162b6b20557c6144ab2d99206f1b30f7872d3
6895mahfuzgit <6895mahfuz@gmail.com>
2016-10-14 18:10:29 +0600

    commit -m "Generated class from rss.xsd  and rss service and also rssexception class  "

---
+++ src/main/java/com/blog/start/exception/RssException.java
@@ -0,0 +1,12 @@
+package com.blog.start.exception;
+
+/**
+ * Created by MahfuzCSE'11 on 14-Oct-16.
+ */
+public class RssException extends Exception {
+
+
+    public RssException(Throwable cause) {
+        super(cause);
+    }
+}
